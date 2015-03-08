db6f2a83af34998a40edc8681e1babf13ee7a7e9
Yukihiro Moriyama <m.y.like.music.10@gmail.com>
2015-03-08 16:53:49 +0900

    MapKitで地図を表示

--- Toilet_Using_MapKit/ViewController.swift
+++ Toilet_Using_MapKit/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    var myMapView : MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // MapViewの生成.
+        myMapView = MKMapView()
+        
+        // MapViewのサイズを画面全体に.
+        myMapView.frame = self.view.bounds
+        
+        // Delegateを設定.
+        myMapView.delegate = self
+        
+        // MapViewをViewに追加.
+        self.view.addSubview(myMapView)
+        
+        // 中心点の緯度経度.
+        let myLat: CLLocationDegrees = 37.506804
+        let myLon: CLLocationDegrees = 139.930531
+        let myCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(myLat, myLon)
+        
+        // 縮尺.
+        let myLatDist : CLLocationDistance = 100
+        let myLonDist : CLLocationDistance = 100
+        
+        // Regionを作成.
+        let myRegion: MKCoordinateRegion = MKCoordinateRegionMakeWithDistance(myCoordinate, myLatDist, myLonDist);
+        
+        // MapViewに反映.
+        myMapView.setRegion(myRegion, animated: true)
     }
 
+    // Regionが変更された時に呼び出されるメソッド.
+    func mapView(mapView: MKMapView!, regionDidChangeAnimated animated: Bool) {
+        println("regionDidChangeAnimated")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
